a194e0f833e902269aa89c70b11f4e23a5ec7007
phuocpro1969 <phuocpro1969@gmail.com>
2022-03-13 01:36:49 +0700

    update eslint and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,35 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    "eslint:recommended",
-    "prettier"
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "tsconfig.json",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
+		"plugin:import/typescript",
+		"eslint:recommended",
+		"prettier",
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+		es2021: true,
+	},
+	ignorePatterns: [".eslintrc.js"],
+	rules: {
+		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		quotes: [
+			"error",
+			"double",
+			{
+				avoidEscape: true,
+			},
+		],
+	},
 };
